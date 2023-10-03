3c92f747b423bb6d6ec6f31b86c01f08927256f5
Sarika Anand <sarikaanand2901@gmail.com>
2023-10-03 12:13:38 +0530

    Day2_

--- src/App.js
+++ src/App.js
@@ -1,44 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import "./App.css";
 import NewCustomerB from "./NewCustomerB";
 import TableRow from "./TableRow";
 import NewCustomerPage from "./NewCustomerPage";
 
-let customers = [
-  {
-    name: "Sarika",
-    phone: "911",
-    email: "bestperson@awesome.com",
-    createdOn: "31 Feb 2051",
-  },
-  {
-    name: "Kajal",
-    phone: "911",
-    email: "bestperson@awesome.com",
-    createdOn: "31 Feb 2051",
-  },
-  {
-    name: "Neeraj",
-    phone: "911",
-    email: "bestperson@awesome.com",
-    createdOn: "31 Feb 2051",
-  },
-  {
-    name: "Rajni",
-    phone: "911",
-    email: "bestperson@awesome.com",
-    createdOn: "31 Feb 2051",
-  },
-  {
-    name: "Sid",
-    phone: "911",
-    email: "bestperson@awesome.com",
-    createdOn: "31 Feb 2051",
-  },
-];
+// let customers = [
+//   {
+//     name: "Sarika",
+//     phone: "911",
+//     email: "bestperson@awesome.com",
+//     createdOn: "31 Feb 2051",
+//   },
+//   {
+//     name: "Kajal",
+//     phone: "911",
+//     email: "bestperson@awesome.com",
+//     createdOn: "31 Feb 2051",
+//   },
+//   {
+//     name: "Neeraj",
+//     phone: "911",
+//     email: "bestperson@awesome.com",
+//     createdOn: "31 Feb 2051",
+//   },
+//   {
+//     name: "Rajni",
+//     phone: "911",
+//     email: "bestperson@awesome.com",
+//     createdOn: "31 Feb 2051",
+//   },
+//   {
+//     name: "Sid",
+//     phone: "911",
+//     email: "bestperson@awesome.com",
+//     createdOn: "31 Feb 2051",
+//   },
+// ];
+
+const CustomerList = (props) => (
+  <>
+    <div className="header-container">
+      <h3>Customers</h3>
+      <NewCustomerB />
+    </div>
+    <div className="table-head-container">
+      <div className="table-head">NAME</div>
+      <div className="table-head">PHONE</div>
+      <div className="table-head">EMAIL</div>
+      <div className="table-head">CREATED ON</div>
+    </div>
+    {props.customers.map((x) => (
+      <TableRow
+        key={x.phone}
+        name={x.name}
+        phone={x.phone}
+        email={x.email}
+        createdOn={x.createdOn}
+      />
+    ))}
+  </>
+);
 
 const App = () => {
+  const [customers, setCustomers] = useState([]);
+  const addCustomer = (newCustomer) => {
+    setCustomers((prevCustomers) => [...prevCustomers, newCustomer]);
+  };
+
   return (
     <Router>
       <div className="app-container">
@@ -57,38 +86,17 @@ const App = () => {
           </div>
         </div>
         <div className="column column-right">
-          <div className="header-container">
-            <h3>Customers</h3>
-            <NewCustomerB />
-          </div>
           <Routes>
-            <Route path="/new-customer" element={<NewCustomerPage />} />
             <Route
-              path="/"
-              element={
-                <>
-                  <div className="table-head-container">
-                    <div className="table-head">NAME</div>
-                    <div className="table-head">PHONE</div>
-                    <div className="table-head">EMAIL</div>
-                    <div className="table-head">CREATED ON</div>
-                  </div>
-                  {customers.map((x) => (
-                    <TableRow
-                      key={x.phone}
-                      name={x.name}
-                      phone={x.phone}
-                      email={x.email}
-                      createdOn={x.createdOn}
-                    />
-                  ))}
-                </>
-              }
+              path="/new-customer"
+              element={<NewCustomerPage onAdd={addCustomer} />}
             />
+            <Route path="/" element={<CustomerList customers={customers} />} />
           </Routes>
         </div>
       </div>
     </Router>
   );
 };
+
 export default App;
--- src/NewCustomerPage.js
+++ src/NewCustomerPage.js
@@ -1,7 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
 
-const NewCustomerPage = () => {
-  return <div>New Customer Page Bla bla bla</div>;
+const NewCustomerPage = ({ onAdd }) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+    email: "",
+  });
+
+  const handleChange = (x) => {
+    const { name, value } = x.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (x) => {
+    x.preventDefault();
+    const newCustomer = {
+      name: formData.name,
+      phone: formData.phone,
+      email: formData.email,
+      createdOn: new Date().toLocaleDateString(),
+    };
+    onAdd(newCustomer);
+    setFormData({ name: "", phone: "", email: "" });
+  };
+
+  return (
+    <div className="container-new-customer">
+      <div className="header-container">
+        <h3>New Customer</h3>
+      </div>
+      <div className="form-box">
+        <form onSubmit={handleSubmit}>
+          <div className="formControls">
+            <label className="formChar">
+              Name
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </label>
+            <label className="formChar">
+              Phone
+              <input
+                type="number"
+                name="phone"
+                value={formData.phone}
+                onChange={handleChange}
+                required
+              />
+            </label>
+            <label className="formChar">
+              Email
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </label>
+            <button className="formChar-submit" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default NewCustomerPage;
